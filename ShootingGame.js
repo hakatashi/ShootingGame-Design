8b0183affe9c1d0386f67ec77b17131419782555
hakatashi <hakatasiloving@gmail.com>
2014-09-13 03:20:24 +0900

    Add player

--- ShootingGame.js
+++ ShootingGame.js
@@ -15,6 +15,11 @@ var Shooting = function () {
 	].map(function (id) {
 		return {id: id, src: 'images/' + id + '.png', type: createjs.LoadQueue.IMAGE};
 	}), true);
+
+	createjs.Ticker.setFPS(60);
+
+	// prevent defaults
+	key('up, down, right, left', function () {return false});
 };
 
 Shooting.prototype.onComplete = function () {
@@ -22,18 +27,22 @@ Shooting.prototype.onComplete = function () {
 	createjs.Ticker.on('tick', this.ticker, this);
 
 	this.background = new Shooting.Background(this);
+	this.player = new Shooting.Player(this);
 };
 
 Shooting.prototype.ticker = function (event) {
 	this.background.ticker(event);
+	this.player.ticker(event);
 	this.stage.update();
 };
 
 Shooting.Background = function (shooting) {
 	this.shooting = shooting;
 
-	this.images = [new createjs.Bitmap(shooting.queue.getResult('background'))];
-	this.shooting.stage.addChild(this.images[0]);
+	this.images = new createjs.Container();
+	this.shooting.stage.addChild(this.images);
+
+	this.images.addChild(new createjs.Bitmap(shooting.queue.getResult('background')));
 
 	this.y = 0;
 };
@@ -47,21 +56,47 @@ Shooting.Background.prototype.ticker = function (event) {
 	if (this.y > 0) {
 		var newImage = new createjs.Bitmap(shooting.queue.getResult('background'));
 
-		this.images.unshift(newImage);
+		this.images.addChild(newImage);
 		this.y -= newImage.image.height;
-		shooting.stage.addChild(newImage);
 	};
 
-	var removes = [];
-
-	this.images.reduce(function (y, image, index) {
+	this.images.children.reduce(function (y, image) {
 		if (y <= shooting.height) {
 			image.y = y;
 		} else {
-			shooting.stage.removeChild(image);
-			background.images.splice(index);
+			background.images.removeChild(image);
 		}
 
 		return y + image.image.height;
 	}, this.y);
+};
+
+Shooting.Player = function (shooting) {
+	this.shooting = shooting;
+
+	this.image = new createjs.Bitmap(shooting.queue.getResult('player'));
+	shooting.stage.addChild(this.image);
+
+	this.x = shooting.width / 2;
+	this.y = shooting.height * 0.75;
+
+	this.v = 300;
+};
+
+Shooting.Player.prototype.ticker = function (event) {
+	if (key.shift) this.v = 100;
+	else this.v = 300;
+
+	var direction = {x: 0, y: 0};
+
+	if (key.isPressed('right')) direction.x += 1;
+	if (key.isPressed('left'))  direction.x -= 1;
+	if (key.isPressed('up'))    direction.y -= 1;
+	if (key.isPressed('down'))  direction.y += 1;
+
+	this.x += direction.x * event.delta / 1000 * this.v;
+	this.y += direction.y * event.delta / 1000 * this.v;
+
+	this.image.x = this.x - this.image.image.width / 2;
+	this.image.y = this.y - this.image.image.height / 2;
 };
